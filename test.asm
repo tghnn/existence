ad8472bf38f1b4ebdf7a96a3d2e28578915efbcf
tghnn <tghnn@bk.ru>
2013-07-25 11:37:41 +0600

    finished

--- test.asm
+++ test.asm
@@ -1,4 +1,4 @@
-format PE64 GUI 5.0
+ format PE64 GUI 5.0
 entry start
 
 include 'win64ax.inc'
@@ -33,56 +33,16 @@ jc eee
 
 f_open_append ftstw,1000,6
 jc eee
+mov rax,746282364
+mov loc_task_int,rax
+f_udec64_write
+f_char_close
 f_open_read ftstr,1000,6
 jc eee
-m_thread_wait 1000
-_oooo:
-m_med_getpnt [file_medium]
-m_task_sour_start rdi,loc_task_file_sour
-jc eee
-m_task_dest_start rdi,loc_task_file_dest
-jc eee
-or r11,r11
-jz _o0
-m_task_sour_ask rdi,loc_task_file_sour,rbx,-1
-jc eee
-or r11,r11
-jz _oo1
-push rbx
-m_task_dest_ask rdi,rsi,loc_task_file_dest,rbx,r11
-pop rdx
-jc eee
-or r11,r11
-jz _oo1
-mov rcx,r11
-__tttt:
-mov al,[rbx]
-mov [rdx],al
-inc rbx
-inc rdx
-loop __tttt
-push r11
-m_task_dest_roger rdi,rsi,loc_task_file_dest,r11
-pop r11
-jc eee
-m_task_sour_roger rdi,loc_task_file_sour,r11
-jc eee
-m_task_dest_finish rdi,loc_task_file_dest
-jc eee
-m_task_sour_finish rdi,loc_task_file_sour
-jc eee
-jmp _oooo
-_oo1:
-m_task_dest_abort rdi,loc_task_file_dest
-jc eee
-m_task_sour_abort rdi,loc_task_file_sour
-jc eee
-m_thread_wait 1000
-jc eee
-jmp _oooo
-_o0:
-m_sour_unset rdi,loc_task_file_sour
-jc eee
+f_dec_read
+mov rax,loc_task_frac
+mov rdx,loc_task_int
+
 invoke Sleep,1000
 invoke	ExitProcess,[msg.wParam]
 
